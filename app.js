c36363bcbc9bba996bfe03df95d23a4350936753
Siddhesh Patel <siddheshpatel1999@gmail.com>
2020-05-24 23:45:44 +0530

    second commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const newsRouter = require('./routes/news.js')
+const testLocal = require('./routes/testLocal')
 
 const app = express();
 
@@ -21,5 +22,6 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news', newsRouter)
+app.use('/testlocal', testLocal)
 
 module.exports = app;
---
+++ routes/testLocal.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+
+const local = {
+    host: 'localhost',
+    port: 5000,
+    method: 'GET'
+};
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    http.request(local, (result) => {
+        result.on('data', chunk => {
+            res.send(chunk);
+        })
+    }).end();
+});
+
+router.get('/suc', function(req, res, next) {
+
+});
+
+module.exports = router;
