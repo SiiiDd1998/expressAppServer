18f231577bc9ad5ff555a6ade25562f1baa5af57
Pratik Prabhu <prabhupratik27@gmail.com>
2020-05-28 18:46:51 +0530

    Connected the server to my mongo atlas

--- dbconnect.js
+++ dbconnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function connect () {
     mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://localhost:27017/hack',{useNewUrlParser: true})
+    mongoose.connect('mongodb+srv://pratik:Pratik@000@cluster0-ao5ty.mongodb.net/test?retryWrites=true&w=majority/hack',{useNewUrlParser: true})
 }
 
 module.exports = connect
